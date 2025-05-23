5785df158dcf4164d8bbce2257f377ad591fe011
Saurav <sauravcoder1@gmail.com>
2025-05-23 21:59:20 +0530

    files added

---
+++ gsap.js
@@ -0,0 +1,152 @@
+const navtl = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.matchMedia({
+    // Desktop (min-width: 768px)
+    "(min-width: 768px)": function () {
+
+        navtl.from("nav .nav-links li", {
+            opacity: 0,
+            y: -20,
+            duration: 0.8,
+            stagger: 0.2,
+            ease: "power3.out"
+        });
+
+        gsap.from("nav .cta-button", {
+            opacity: 0,
+            y: -20,
+            duration: 1,
+            delay: 1.2,
+            ease: "power4.out"
+        });
+
+
+        gsap.from(".hero .logo span", {
+            x: -45,
+            opacity: 0,
+            duration: 1,
+            stagger: 0.1,
+        })
+        gsap.from(".hero h1", {
+            x: -45,
+            opacity: 0,
+            duration: 1,
+        })
+        gsap.from(".hero p", {
+            y: 50,
+            opacity: 0,
+            duration: 0.8,
+            delay: 0.7,
+            stagger: 0.1,
+        })
+        gsap.from(".hero .hero-buttons .d", {
+            y: 20,
+            opacity: 0,
+            duration: 0.8,
+            delay: 0.8,
+            stagger: 0.1,
+        })
+        gsap.from(".hero img", {
+            x: 50,
+            scale: 0.9,
+            opacity: 0,
+            duration: 0.8,
+            delay: 0.8,
+            stagger: 0.1,
+        })
+    }
+})
+
+ScrollTrigger.matchMedia({
+    // Desktop (min-width: 768px)
+    "(max-width: 768px)": function () {
+        gsap.from(".hero .logo span", {
+            y: 20,
+            opacity: 0,
+            duration: 0.5,
+            // stagger: 0.1,
+        })
+        gsap.from(".hero .hero-content h1", {
+            y: 20,
+            opacity: 0,
+            duration: 0.5,
+            delay: 0.2,
+            // stagger: 0.1,
+        })
+        gsap.from(".hero .hero-content p", {
+            y: 20,
+            opacity: 0,
+            duration: 0.5,
+            delay: 0.4,
+            // stagger: 0.1,
+        })
+        gsap.from(".hero .hero-buttons a", {
+            y: 20,
+            opacity: 0,
+            duration: 0.5,
+            delay: 0.5,
+            stagger: 0.1,
+        })
+    }
+});
+
+gsap.from(".service-card", {
+    scale: 0.95,
+    opacity: 0.5,
+    y: 80,
+    stagger: 0.1,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".service-card",
+        start: "top 80%",
+        end: "top 40%",
+        scrub: 1,
+    }
+});
+
+gsap.from(".portfolio-item", {
+    scale: 0.95,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".portfolio-item",
+        scroller: "body",
+        start: "top 80%",
+        end: "top 40%",
+    }
+});
+
+gsap.from(".testimonial-card", {
+    scale: 0.95,
+    opacity: 0,
+    y: 50,
+    stagger: 0.2,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".testimonial-card",
+        scroller: "body",
+        start: "top 80%",
+        end: "top 40%",
+    }
+});
+
+gsap.from(".contact-icon i", {
+    scale: 0.95,
+    opacity: 0,
+    x: -50,
+    stagger: 0.1,
+    duration: 1,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: ".contact-text h3",
+        scroller: "body",
+        start: "top 80%",
+        end: "top 40%",
+    }
+});
---
+++ script.js
@@ -0,0 +1,50 @@
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+const mobileMenu = document.getElementById('mobileMenu');
+
+mobileMenuBtn.addEventListener('click', function () {
+  mobileMenu.classList.toggle('active');
+  this.innerHTML = mobileMenu.classList.contains('active') ?
+    '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
+});
+document.querySelector(".body").addEventListener("click", () => {
+  // mobileMenu.style.transform = "translateY(-150%)";
+  mobileMenu.classList.remove('active');
+  mobileMenuBtn.innerHTML = mobileMenu.classList.contains('active') ?
+    '<i class="fas fa-times"></i>' : '<i class="fas fa-bars"></i>';
+
+})
+// Close mobile menu when clicking a link
+document.querySelectorAll('.mobile-menu-links a').forEach(link => {
+  link.addEventListener('click', function () {
+    mobileMenu.classList.remove('active');
+    mobileMenuBtn.innerHTML = '<i class="fas fa-bars"></i>';
+  });
+});
+
+const button = document.querySelector(".cta-button");
+
+// On hover, animate up
+button.addEventListener("mouseenter", () => {
+  gsap.to(button, { y: -5, duration: 0.5, ease: "power2.out" });
+});
+
+// On mouse leave, animate back
+button.addEventListener("mouseleave", () => {
+  gsap.to(button, { y: 0, duration: 0.5, ease: "power2.out" });
+});
+
+// logo
+const h1 = document.querySelector(".hero .hero-content .logo");
+let spans = "";
+const array = h1.textContent.split("");
+array.forEach((ele, idx) => {
+  if (idx < (array.length / 2)) {
+    spans += `<span class="a">${ele}</span>`;
+  } else {
+    spans += `<span class="b">${ele}</span>`;
+  }
+  if (idx >= 5) {
+    spans += `<span class="b" style="color: var(--secondary);>${ele}</span>`;
+  }
+});
+h1.innerHTML = spans;
